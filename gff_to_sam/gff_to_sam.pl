d6a5f19ff4968c6bcb4a2c576100d9d49ed40725
yostinso <yostinso>
2009-09-24 23:19:37 +0000

    Use better hit syntax, I think

--- gff_to_sam/gff_to_sam.pl
+++ gff_to_sam/gff_to_sam.pl
@@ -7,21 +7,21 @@ my $filesize = -s $ARGV[0];
 
 # Header
 print <<EOF
-\@SQ	SN:chr2L	AS:FlyBase r5	LN:23011544	SP:Drosophila melanogaster
-\@SQ	SN:chr2LHet	AS:FlyBase r5	LN:368872	SP:Drosophila melanogaster
-\@SQ	SN:chr2R	AS:FlyBase r5	LN:21146708	SP:Drosophila melanogaster
-\@SQ	SN:chr2RHet	AS:FlyBase r5	LN:3288761	SP:Drosophila melanogaster
-\@SQ	SN:chr3L	AS:FlyBase r5	LN:24543557	SP:Drosophila melanogaster
-\@SQ	SN:chr3LHet	AS:FlyBase r5	LN:2555491	SP:Drosophila melanogaster
-\@SQ	SN:chr3R	AS:FlyBase r5	LN:27905053	SP:Drosophila melanogaster
-\@SQ	SN:chr3RHet	AS:FlyBase r5	LN:2517507	SP:Drosophila melanogaster
-\@SQ	SN:chr4	AS:FlyBase r5	LN:1351857	SP:Drosophila melanogaster
-\@SQ	SN:chrX	AS:FlyBase r5	LN:22422827	SP:Drosophila melanogaster
-\@SQ	SN:chrXHet	AS:FlyBase r5	LN:204112	SP:Drosophila melanogaster
-\@SQ	SN:chrYHet	AS:FlyBase r5	LN:347038	SP:Drosophila melanogaster
-\@SQ	SN:chrM	AS:FlyBase r5	LN:19517	SP:Drosophila melanogaster
-\@SQ	SN:chrU	AS:FlyBase r5	LN:10049159	SP:Drosophila melanogaster
-\@SQ	SN:chrUextra	AS:FlyBase r5	LN:29004788	SP:Drosophila melanogaster
+\@SQ	SN:2L	AS:FlyBase r5	LN:23011544	SP:Drosophila melanogaster
+\@SQ	SN:2LHet	AS:FlyBase r5	LN:368872	SP:Drosophila melanogaster
+\@SQ	SN:2R	AS:FlyBase r5	LN:21146708	SP:Drosophila melanogaster
+\@SQ	SN:2RHet	AS:FlyBase r5	LN:3288761	SP:Drosophila melanogaster
+\@SQ	SN:3L	AS:FlyBase r5	LN:24543557	SP:Drosophila melanogaster
+\@SQ	SN:3LHet	AS:FlyBase r5	LN:2555491	SP:Drosophila melanogaster
+\@SQ	SN:3R	AS:FlyBase r5	LN:27905053	SP:Drosophila melanogaster
+\@SQ	SN:3RHet	AS:FlyBase r5	LN:2517507	SP:Drosophila melanogaster
+\@SQ	SN:4	AS:FlyBase r5	LN:1351857	SP:Drosophila melanogaster
+\@SQ	SN:X	AS:FlyBase r5	LN:22422827	SP:Drosophila melanogaster
+\@SQ	SN:XHet	AS:FlyBase r5	LN:204112	SP:Drosophila melanogaster
+\@SQ	SN:YHet	AS:FlyBase r5	LN:347038	SP:Drosophila melanogaster
+\@SQ	SN:M	AS:FlyBase r5	LN:19517	SP:Drosophila melanogaster
+\@SQ	SN:U	AS:FlyBase r5	LN:10049159	SP:Drosophila melanogaster
+\@SQ	SN:Uextra	AS:FlyBase r5	LN:29004788	SP:Drosophila melanogaster
 EOF
 ;
 
@@ -49,8 +49,8 @@ while (my $line = <GFF>) {
         my ($read_id, $strand, $seq, $start) = @$hit;
         $read_id .= "_$sam_idx";
         my $num_matches = "NH:i:" . scalar(@hits_for_this_sequence);
-        my $hits = "h" x length($sequence);
-        print join("\t", $read_id, $strand, $seq, $start, "0", $seq_length, "*", "0", "0", $sequence, $hits, $num_matches) . "\n";
+        #my $hits = "h" x length($sequence);
+        print join("\t", $read_id, $strand, $seq, $start, "255", $seq_length, "*", "0", "0", $sequence, "*", $num_matches) . "\n";
       }
     }
     ($sequence) = ($attrs =~ /ID=Sequence:([^;]*)/);
@@ -73,8 +73,8 @@ for (my $i = 0; $i < $old_score; $i++) {
     my ($read_id, $strand, $seq, $start) = @$hit;
     $read_id .= "_$sam_idx";
     my $num_matches = "NH:i:" . scalar(@hits_for_this_sequence);
-    my $hits = "h" x length($sequence);
-    print join("\t", $read_id, $strand, $seq, $start, "0", $seq_length, "*", "0", "0", $sequence, $hits, $num_matches) . "\n";
+    #my $hits = "h" x length($sequence);
+    print join("\t", $read_id, $strand, $seq, $start, "255", $seq_length, "*", "0", "0", $sequence, "*", $num_matches) . "\n";
   }
 }
 
