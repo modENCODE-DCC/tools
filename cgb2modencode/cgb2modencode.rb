8db16a91a0d44b52ef08ba4736846448e3a31427
nlw <nlw>
2012-01-23 23:14:52 +0000

    modified cgb2modencode script to add a qc image url to the dbfields template

--- cgb2modencode/cgb2modencode.rb
+++ cgb2modencode/cgb2modencode.rb
@@ -13,6 +13,7 @@ DEFAULT_INI_FILE="cgb-wiki-bot.yml"
 DEFAULT_CACHE_FILE="cgb.cache"
 DEFAULT_CACHE_AGE=86400
 NEVER_USE_CACHE="true"
+WIKI_PREFIX="http://wiki.modencode.org/project/index.php?title="
 
 ini_file = ARGV[0] || File.join(File.dirname(__FILE__), DEFAULT_INI_FILE)
 @config = File.open(ini_file) { |f| YAML.load(f) }
@@ -22,7 +23,7 @@ cache_file = @config["cache_file"] || File.join(File.dirname(__FILE__), DEFAULT_
 if (File.exists?(cache_file)) then
   cache_age = Time.now - File.mtime(cache_file)
   max_cache_age = @config["max_cache_age"] || DEFAULT_CACHE_AGE
-  File.unlink(cache_file) if (cache_age >= max_cache_age) || NEVER_USE_CACHE
+  #File.unlink(cache_file) if (cache_age >= max_cache_age) || NEVER_USE_CACHE
 end
 
 cgbb = nil
@@ -46,15 +47,21 @@ if !File.exists?(cache_file) then
 
   abs.map! { |ab|
     rna_id = ab["RNA ID"].match(/\*\}?(\d+)\*/)[1] # ID looks like: "{anchor:170}{*}170*" or "{anchor:170} *170*"
+    #puts "parsing #{rna_id}"
     # Old version -- no longer works
     #rna_id = ab["RNA ID"].match(/\*(\d+)\*/)[1]
     bs_id = ab["Biological sample ID"].match(/\d+/)[0]
     cell_type = ab["sample"].gsub(/^\W+|\|.*/, '').gsub(/\\\+/, '+')
     qc_pages = ab["RNA QC"].split(/\s*,\s*/).map { |qc| qc.gsub(/^\[|\]$/, '').gsub(/\\\+/, '+').split(/\|/) } if ab["RNA QC"]
+    puts "#{ab.inspect()}"
+    histogram_pages = ab["biological QC"].split(/\s*,\s*/).map { |qc| qc.gsub(/^\[|\]$/, '').gsub(/\\\+/, '+').gsub(/\\-/,'').split(/\|/)} if ab["biological QC"]
+    qc_pages += histogram_pages if (ab["RNA QC"] && ab["biological QC"])
     prep_page = ab["preparation method"].gsub(/^\[|\]$/, '').gsub(/\\\+/, '+').split(/\|/) if ab["preparation method"]
     ab[:qc_pages] ||= []
-
-    { :rna_id => rna_id, :bs_id => bs_id, :cell_type => cell_type, :qc_pages => qc_pages, :prep_page => prep_page }
+    qc_pages.delete_if{|x| x == []} unless qc_pages.nil?
+    qc_image1 = ""
+    qc_image2 = ""
+    { :rna_id => rna_id, :bs_id => bs_id, :cell_type => cell_type, :qc_pages => qc_pages, :prep_page => prep_page, :qc_image1 => "", :qc_image2 => ""}
   }
   File.open(cache_file, "w") { |f| Marshal.dump(abs, f) }
 else
@@ -69,7 +76,7 @@ dbf = DbfieldsBot.new(@config["dbfields"]["uri"])
 
 # Verify wiki synchronization
 #for testing use the following line, instead of looping
-#ab = abs.find{|x| x[:rna_id] == "634"}
+#test_abs = [abs.find{|x| x[:rna_id] == "257"}]
 #or use the following three lines to test a subset
 #ids = (630..635).to_a
 #test_abs = ids.map{|id| abs.find{|x| x[:rna_id] == id.to_s}}
@@ -77,7 +84,7 @@ dbf = DbfieldsBot.new(@config["dbfields"]["uri"])
 abs.compact.each { |ab|
   puts "Checking sample #{ab[:rna_id]} for up-to-dateness."
   cgbb_counter += 1
-  puts "counter: #{cgbb_counter}"
+  #puts "counter: #{cgbb_counter}"
   # Build template page from existing page, if any
   # Also fetch DBFields form content, if any
   title = "Celniker/RNA:#{ab[:rna_id]}"
@@ -101,7 +108,7 @@ abs.compact.each { |ab|
   cgb_page.scraped_data = [ 
     "RNA ID: #{ab[:rna_id]}",
     "Biosample: #{ab[:bs_id]}",
-    "Cell type: #{ab[:cell_type]}"
+    "Sample Name: #{ab[:cell_type]}"
   ].join("\n")
 
   # Generate protocol links
@@ -118,16 +125,24 @@ abs.compact.each { |ab|
       puts "  Using #{page}|#{desc} since there's no mapping for #{p[0]}"
     end
     # Two different link formats: the first is for external links, the second is internal links
-    m.nil? ? "[#{page} #{desc}]" : "[[#{page}|#{desc}]]"
+    m.nil? ? "[#{desc} #{page}]" : "[[#{page}|#{desc}]]"
   }.map { |p| p.sub(/^/, '*') }.join("\n")
 
   # Generate QC image links
   # This is a quick check to see if the QC pages match
   # We just check the link text so we don't have to go get the filename from CGB (which is extra page loads)
+  #puts "#{ab.inspect()}"
   cgb_qc_images = ab[:qc_pages].to_a.map { |p| p[1] }.sort
-  me_qc_images = me_page.qcdata.split(/\n+/).map { |q| p = q.gsub(/^\*\[\[|\]\]$/, '').split(/\|/)[1] }.sort unless me_page.qcdata.nil?
+  #puts "#{me_page.inspect()}"
+  #me_qc_images = me_page.qcdata.split(/\n+/).map { |q| p = q.gsub(/^\*\[\[|\]\]$/, '').split(/\|/)[1] }.sort unless me_page.qcdata.nil?
+  me_qc_images = me_page.qcdata.split(/\n/).map { |q| q.gsub(/^\{/,'').gsub(/\s*\[\[/,'').gsub(/\]\]$/,'').gsub(/\}/,'').split(/\|/)[1] }.compact.sort unless me_page.qcdata.nil?
   # If the link text is correct, then just go ahead and assume the image files are correct
   cgb_page.qcdata = me_page.qcdata if ((me_qc_images == cgb_qc_images) && @config["lazy_qc_checks"])
+  #puts "#{me_qc_images.inspect()}"
+
+  #we are hard-coding to allow up to two QC images per sample as per the wiki template
+  qc_image1 = WIKI_PREFIX + me_qc_images[0] unless me_page.qcdata.nil? || me_qc_images[0].nil?
+  qc_image2 = WIKI_PREFIX + me_qc_images[1] unless me_page.qcdata.nil? || me_qc_images[1].nil?
 
   # Check the templates against each other
   if ( cgb_page.scraped_data == me_page.scraped_data && cgb_page.protocols == me_page.protocols &&
@@ -178,22 +193,24 @@ abs.compact.each { |ab|
             puts "    Fetched image #{img_name} from cgbb page #{p[1]}."
           }
           raise RuntimeException.new("Unable to upload file; nil response object from wiki") if res_file.nil?
-          "[[Image:#{res_file}|#{p[1]}]]"
+          "[[Image:#{res_file}|thumb|300px|#{p[1]}]]"
         else
           puts "    No QC image found on page #{p[1]}"
-          "[[Image:No_QC_image|#{p[1]}]]"
+          "[[Image:No_QC_image|thumb|300px|#{p[1]}]]"
         end
-      }.join("\n")
-      me_page.qcdata = cgb_page.qcdata
+      }.join("\n| ")
+      me_page.qcdata = "{|\n| " + cgb_page.qcdata + "\n|}\n"
     end
     me_page.scraped_data = cgb_page.scraped_data
     me_page.protocols = cgb_page.protocols
-
+    ab[:qc_image1] = qc_image1
+    ab[:qc_image2] = qc_image2
     # Post new text
     res = me.update_page(title, me_page.content)
   end
 
-  cgb_form = { "RNA ID" => ab[:rna_id], "Official Name" => ab[:cell_type], "Biosample #" => ab[:bs_id] }
+  cgb_form = { "RNA ID" => ab[:rna_id], "Official Name" => ab[:cell_type], "Biosample #" => ab[:bs_id], "QC image1" => ab[:qc_image1], "QC image2" => ab[:qc_image2] }
+  #puts "#{cgb_form.inspect}"
   if (me_form == cgb_form) then
     puts "  DBFields content matches."
   else
